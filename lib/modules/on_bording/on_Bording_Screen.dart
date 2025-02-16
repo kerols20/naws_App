324e2c5475817bd6c91d0d1c8d996dd1234eb721
Kerolos <kokoqaw4@gmail.com>
2025-02-16 15:32:39 +0200

    Edit level one

--- lib/modules/on_bording/on_Bording_Screen.dart
+++ lib/modules/on_bording/on_Bording_Screen.dart
@@ -4,13 +4,15 @@ import 'package:naws_app/core/models/catagory_widget.dart';
 import 'package:naws_app/modules/on_bording/widget_on_Bording/widget.dart';
 
 class on_Bording extends StatefulWidget {
-   on_Bording({super.key});
+   on_Bording({super.key, });
 
   @override
   State<on_Bording> createState() => _on_BordingState();
 }
 
 class _on_BordingState extends State<on_Bording> {
+
+  catgory_widget? _salecteditem; // null
   List<catgory_widget> catgoryList = [
     catgory_widget(catgoryId: "General", catgoryname: "General", catgoryIamge: "assets/images/Frame 10.png"),
     catgory_widget(catgoryId: "Business", catgoryname: "Business", catgoryIamge: "assets/images/Frame 11.png"),
@@ -23,37 +25,24 @@ class _on_BordingState extends State<on_Bording> {
     return Scaffold(
       drawer: Drawer(),
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text(_salecteditem == null? "Categories": _salecteditem!.catgoryname),
         actions: [
           IconButton(onPressed: (){}, icon: Icon(Icons.search))
         ],
         centerTitle: true,
       ),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Good Morning\n Here is some naws for you", style: theme.textTheme.headlineMedium),
-            ),
-            10.spaceVertcial,
-            ListView.separated(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-                itemBuilder: (context, index) => Stack(
-                  alignment: index%2 == 0 ? Alignment.centerRight : Alignment.centerLeft,
-                  children: [
-                    Image.asset(catgoryList[index].catgoryIamge,),
-                    Padding(
-                      padding:  EdgeInsets.only(top: 105, right: index % 2 == 0 ? 10 : 0, left: index % 2 == 0 ? 0 : 10),
-                      child:  index%2 == 0 ? widgetRight() : WidgetLeft(),
-                    )
-                  ],
-                ), separatorBuilder: (context, index) => SizedBox(height: 10,), itemCount: catgoryList.length)
-          ],
-        ),
+      body: _salecteditem == null? categroy_Home_Viwe(
+        onTap: oncategreycliced,
+      ): slecated_Home_category_viwe(
+        catgory: _salecteditem!,
       ),
     );
+  }
+  void oncategreycliced(catgory_widget catgory){
+    setState(() {
+      _salecteditem = catgory;
+    });
+    print(catgory.catgoryId);
   }
 }
--- lib/modules/on_bording/widget_on_Bording/widget.dart
+++ lib/modules/on_bording/widget_on_Bording/widget.dart
@@ -1,94 +1,100 @@
 import 'package:flutter/material.dart';
-
-class widgetRight extends StatelessWidget {
-  widgetRight({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    return Container(
-      width: 200,
-      height: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(50),
-        color: Colors.black.withOpacity(0.5),
-      ),
-      child: Row(
-        children: [
-          GestureDetector(
-            onTap: () {
-              //x/x/x/x/x/x/x/
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text("View All ", style: theme.textTheme.headlineMedium?.copyWith(color: Colors.white),),
-            ),
-          ),
-          Spacer(),
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.black,
-            ),
-            alignment: Alignment.center,
-            child: Icon(
-              Icons.arrow_forward_ios,
-              color: Colors.white,
-              size: 24,
-            ),
-          ),
-        ],
-      )
-
-    );
-  }
-}
-class WidgetLeft extends StatelessWidget {
-  const WidgetLeft({super.key});
+import 'package:naws_app/core/models/catagory_widget.dart';
+class categroy_Home_Viwe extends StatelessWidget {
+  final void Function(catgory_widget) onTap;
+   categroy_Home_Viwe({super.key , required this.onTap});
 
   @override
   Widget build(BuildContext context) {
+    catgory_widget? _salecteditem; // null
+    List<catgory_widget> catgoryList = [
+      catgory_widget(catgoryId: "General", catgoryname: "General", catgoryIamge: "assets/images/Frame 10.png"),
+      catgory_widget(catgoryId: "Business", catgoryname: "Business", catgoryIamge: "assets/images/Frame 11.png"),
+      catgory_widget(catgoryId: "Sports", catgoryname: "Sports", catgoryIamge: "assets/images/Frame 12.png"),
+      catgory_widget(catgoryId: "Health", catgoryname: "Health", catgoryIamge: "assets/images/Frame 12 (1).png"),
+    ];
     var theme = Theme.of(context);
-    return Container(
-      width: 200,
-      height: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(50),
-        color: Colors.black.withOpacity(0.5),
-      ),
-      child: Row(
-        children: [
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.black,
+    return  SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0), // إضافة تباعد خارجي
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Good morning\n Here some news for you",
+              style: theme.textTheme.headlineSmall,
             ),
-            alignment: Alignment.center,
-            child: Icon(
-              Icons.arrow_back_ios,
-              color: Colors.white,
-              size: 24,
-            ),
-          ),
-          Spacer(),
-          GestureDetector(
-            onTap: () {
-              // /x/x/x/x/x/x
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                "View All",
-                style: theme.textTheme.headlineMedium?.copyWith(color: Colors.white),
+            SizedBox(height: 20),
+            ListView.separated(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) => Stack(
+                alignment: index % 2 == 0 ? Alignment.bottomRight : Alignment.bottomLeft,
+                children: [
+                  Image.asset(catgoryList[index].catgoryIamge),
+                  Container(
+                    width: 200,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                    child: Directionality(
+                      textDirection: index % 2 == 0 ? TextDirection.ltr : TextDirection.rtl,
+                      child: Row(
+                        children: [
+                           GestureDetector(
+                             onTap: (){
+                               onTap(catgoryList[index]);
+                             },
+                             child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  "View All",
+                                  style: theme.textTheme.headlineMedium?.copyWith(color: Colors.white),
+                                ),
+                              ),
+                           ),
+                          Spacer(),
+                          Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.black,
+                            ),
+                            alignment: Alignment.center,
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.white,
+                              size: 24,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
+              separatorBuilder: (context, index) => SizedBox(height: 10),
+              itemCount: catgoryList.length,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
+class slecated_Home_category_viwe extends StatelessWidget {
+final catgory_widget catgory;
+   slecated_Home_category_viwe({super.key, required this.catgory});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
+
